66666aaa577bb3ffed13284d2d0d8e3cea4f2bf9
joannafawl <joanna.fawl@madetech.com>
2020-09-14 14:59:03 +0100

    create 0 - 0 test

--- spec/gamemanager_spec.rb
+++ spec/gamemanager_spec.rb
@@ -1,10 +1,20 @@
 require_relative '../lib/GameManager'
 
 describe GameManager do
+
+    let!(:gm) { GameManager.new }
+
   context "Creates games" do
      it "Initiates a new game" do
-        gm = GameManager.new
+        expect(gm.newGame)
+     end
+   end
+
+  context "Creates games" do
+     it "New game score 0 - 0" do
         expect(gm.newGame)
      end
   end
+
+  
 end
