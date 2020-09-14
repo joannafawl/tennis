11445678607ac0e30391ba22d4771e0594a91bb5
joannafawl <joanna.fawl@madetech.com>
2020-09-14 16:08:58 +0100

    Test 30 - 15

--- spec/gamemanager_spec.rb
+++ spec/gamemanager_spec.rb
@@ -2,7 +2,6 @@ require_relative '../lib/GameManager'
 
 describe GameManager do
     let!(:gm) { GameManager.new() }
-    #let!(:player_one) { GameManager.new() }
 
   context "Creates games" do
      it "New game score 0 - 0" do
@@ -12,9 +11,22 @@ describe GameManager do
 
   context "Update game" do
     it "Game score 15 - 0" do
-       gm.add_point("player_one")
+       gm.add_point(:player_one)
        expect(gm.get_score).to eq ({ player_one: 15, player_two: 0 })
     end
+
+    it "Game score 15 - 15" do
+        gm.add_point(:player_one)
+        gm.add_point(:player_two)
+        expect(gm.get_score).to eq ({ player_one: 15, player_two: 15 })
+     end
+
+     it "Game score 30 - 15" do
+      gm.add_point(:player_one)
+      gm.add_point(:player_two)
+      gm.add_point(:player_one)
+      expect(gm.get_score).to eq ({ player_one: 30, player_two: 15 })
+   end
  end
 
  context "Decide point winner" do
