92834b8eae24bfd42e04884ec4ce1ccc0c19c23b
joannafawl <joanna.fawl@madetech.com>
2020-09-14 15:16:52 +0100

    Pass new game score 0 - 0 test

--- lib/GameManager.rb
+++ lib/GameManager.rb
@@ -1,7 +1,15 @@
 class GameManager
 
-    def newGame
-        true
+    def initialize()
+        @score = { player_one: 0, player_two: 0 }
+    end
+
+    def add_player
+        # TODO
+    end
+
+    def get_score
+        @score
     end
 
 end
